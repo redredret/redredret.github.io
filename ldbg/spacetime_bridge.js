7d52c38c2dc4880025496ccf9e140287cffbf664
redredret <redredret@gmail.com>
2026-08-23 20:59:33 -0400

    Deploy LDBG browser build

--- ldbg/spacetime_bridge.js
+++ ldbg/spacetime_bridge.js
@@ -8442,31 +8442,10 @@ ${ty.variants.map(
     seed: t.u32()
   };
 
-  // src/module_bindings/submit_farm_run_result_v_2_reducer.ts
-  var submit_farm_run_result_v_2_reducer_default = {
-    runId: t.string(),
-    score: t.u32(),
-    linesCleared: t.u32(),
-    enemiesDefeated: t.u32(),
-    completed: t.bool(),
-    clearGroups: t.string(),
-    garbageBlocksCleared: t.u32()
-  };
-
   // src/module_bindings/submit_farm_run_result_v_3_reducer.ts
   var submit_farm_run_result_v_3_reducer_default = {
     runId: t.string(),
     trace: t.string()
-  };
-
-  // src/module_bindings/submit_run_result_v_2_reducer.ts
-  var submit_run_result_v_2_reducer_default = {
-    runId: t.string(),
-    score: t.u32(),
-    linesCleared: t.u32(),
-    enemiesDefeated: t.u32(),
-    completed: t.bool(),
-    garbageBlocksCleared: t.u32()
   };
 
   // src/module_bindings/unequip_inventory_item_reducer.ts
@@ -8709,9 +8688,7 @@ ${ty.variants.map(
     reducerSchema("send_farm_pvp_attack", send_farm_pvp_attack_reducer_default),
     reducerSchema("set_display_name", set_display_name_reducer_default),
     reducerSchema("start_run", start_run_reducer_default),
-    reducerSchema("submit_farm_run_result_v_2", submit_farm_run_result_v_2_reducer_default),
     reducerSchema("submit_farm_run_result_v_3", submit_farm_run_result_v_3_reducer_default),
-    reducerSchema("submit_run_result_v_2", submit_run_result_v_2_reducer_default),
     reducerSchema("unequip_inventory_item", unequip_inventory_item_reducer_default)
   );
   var proceduresSchema = procedures();
